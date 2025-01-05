e039b6b78c9968df4b71cef95547948f4cd9c785
nazareno1010 <nazarenobeauvais@gmail.com>
2025-01-05 11:51:58 -0300

    Added grained.js

---
+++ script.js
@@ -0,0 +1,9 @@
+grained('#container', {
+    animate: true,
+    patternWidth: 1000,
+    patternHeight: 1000,
+    grainOpacity: 0.1,
+    grainDensity: 1,
+    grainWidth: 0.5,
+    grainHeight: 0.5
+});
